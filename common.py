635e19124ba74cdd9d28576d6ad88a68783be0fb
FreeBirdLjj <ljj11011@mail.ustc.edu.cn>
2014-01-25 14:32:09 +0800

    Add missop()

--- common.py
+++ common.py
@@ -8,9 +8,14 @@ def version(prog):
 def opterr(prog, wrngopt):
     print("%s: invalid option --\'%s\'" % (prog, wrngopt.opt), file=sys.stderr)
     print("Try \'%s --help\' for more information." % (prog), file=sys.stderr)
-    exit(-1)
+    exit(1)
 
 
 def ferr(prog, file):
     print("%s: %s: No such file or directory" % (prog, file), file=sys.stderr)
     exit(2)
+
+def missop(prog):
+    print("%s: missing operand" % (prog), file=sys.stderr)
+    print("Try \'%s --help\' for more information." % (prog), file=sys.stderr)
+    exit(1)
